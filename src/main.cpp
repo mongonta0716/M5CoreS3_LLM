c64fdeb30b740371bb1b5d3664994333e177d860
Takao Akaki <mongonta@gmail.com>
2025-01-24 17:56:30 +0900

    1st commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@
 #include <Arduino.h>
 #include <M5Unified.h>
 #include <M5ModuleLLM.h>
+#include "AquesTalkTTS.h" // AquesTalk-ESP32のラッパークラス
 
 M5ModuleLLM module_llm;
 String llm_work_id;
+
+String talk_str = ""; // AquesTalkで喋る文字列
 
 void setup()
 {
@@ -19,10 +22,12 @@ void setup()
 
     /* Init module serial port */
     Serial.begin(115200);
+
     //Serial2.begin(115200, SERIAL_8N1, 16, 17);  // Basic
     //Serial2.begin(115200, SERIAL_8N1, 13, 14);  // Core2
-    Serial2.begin(115200, SERIAL_8N1, 18, 17);  // CoreS3
-
+    //Serial2.begin(115200, SERIAL_8N1, 18, 17);  // CoreS3
+    Serial2.begin(115200, SERIAL_8N1, M5.getPin(m5::pin_name_t::port_c_rxd), M5.getPin(m5::pin_name_t::port_c_txd));  // Port.C
+    //Serial2.begin(115200, SERIAL_8N1, 13, 12);  // Fire Custom
     /* Init module */
     module_llm.begin(&Serial2);
 
@@ -42,7 +47,21 @@ void setup()
     M5.Display.printf(">> Setup llm..\n");
     llm_work_id = module_llm.llm.setup();
     Serial.print("Initialized...");
+    // AquesTalkの初期化
+    uint8_t mac[6];
+    esp_efuse_mac_get_default(mac);
+    Serial.printf("¥nMAC Address = %02X:%02X:%02X:%02X:%02X:%02X¥r¥n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    int err = TTS.createK();
+    if (err) {
+        M5_LOGI("ERR:TTS.createK(): %d", err);
+    }
+    M5.Speaker.setVolume(128);
+    TTS.playK("こんにちは、漢字を混ぜた文章です。");
+    TTS.wait();   // 終了まで待つ
+
 }
+
+
 
 void loop()
 {
@@ -60,9 +79,15 @@ void loop()
       /* Push question to LLM module and wait inference result */
       module_llm.llm.inferenceAndWaitResult(llm_work_id, question.c_str(), [](String& result) {
           /* Show result on screen */
+          talk_str += result.c_str();
           M5.Display.printf("%s", result.c_str());
           Serial.printf("%s", result.c_str());
       });
+      M5.Display.println(llm_work_id);
+      M5_LOGI("文字列:%s", talk_str.c_str());
+      TTS.playK(talk_str.c_str(), 100);
+      TTS.wait();
+      talk_str = "";
       M5.Display.println();
     }
 
